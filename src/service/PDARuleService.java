56330bb1d14f76d9e268da5f8facfe4e28ebd853
joaoromaniello <jaromaniello@gmail.com>
2021-05-25 14:02:37 -0300

    PDAJson added

--- src/service/PDARuleService.java
+++ src/service/PDARuleService.java
@@ -8,6 +8,8 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 
+//TODO
+
 public class PDARuleService {
 
     PDARuleRepository ruleRepository = new PDARuleRepository(); //cria um novo repositorio de regras
